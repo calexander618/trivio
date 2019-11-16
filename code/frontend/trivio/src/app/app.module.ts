07a7e38a408532d92896218952483d8b4e217f84
Deimious <baalnaroz@tutanota.com>
2019-11-16 13:09:35 +0000

    made some bare components just to start with

--- code/frontend/trivio/src/app/app.module.ts
+++ code/frontend/trivio/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TitlebarComponent } from './titlebar/titlebar.component';
+import { LoginComponent } from './login/login.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ScorecardComponent } from './scorecard/scorecard.component';
+import { DifficultyComponent } from './difficulty/difficulty.component';
+import { CategoryComponent } from './category/category.component';
+import { TriviaCardComponent } from './trivia-card/trivia-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitlebarComponent,
+    LoginComponent,
+    QuestionsComponent,
+    ScorecardComponent,
+    DifficultyComponent,
+    CategoryComponent,
+    TriviaCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ code/frontend/trivio/src/app/trivia-card/trivia-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trivia-card',
+  templateUrl: './trivia-card.component.html',
+  styleUrls: ['./trivia-card.component.css']
+})
+export class TriviaCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
