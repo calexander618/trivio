5cc57d78119f99d2b2ed2cfcb90ee816221545dd
Michael Woodruff <michaelwoodruff@Michaels-MacBook-Pro.local>
2019-11-16 15:50:23 -0600

    basic mongoose implementation to connect to local mongodb called secure-auth, bcrypt for hashing password

---
+++ code/server/Model/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String, 
+        required: true, 
+        unique: 1, 
+        trim: true
+    }, 
+    password: {
+        type: String, 
+        required: true, 
+        minlength: 6
+    }
+});
+
+let SALT = 10;
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(SALT, function(err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User }
--- code/server/index.js
+++ code/server/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 const app = express();
 const port = 3000;
+const { User } = require('./Model/User');
 
-app.use(express.static('public'));
+const mongurl = 'mongodb://127.0.0.1:27017/sec-auth';
+
+mongoose.connect(mongurl)
+    .then(() => 'connected to mongo')
+    .catch(error => console.log(error));
+
+app.use(bodyparser.json());
+
+app.post('/api/user/signup', (req, res) => {
+    const user = new User({
+        email: req.body.email, 
+        password: req.body.password
+    }).save((err, response) => {
+        if (err) res.status(400).send(err);
+        res.status(200).send(response);
+    });
+});
+
+app.use('/', express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
