d2930af6b5b9f8fbb5ac3fe6e6a0ebb40b08ef61
aceprogram <calexander618@gmail.com>
2019-11-19 22:17:10 -0600

    added questions and timer

--- code/frontend-vue/src/views/DashboardPages/GamePage.vue
+++ code/frontend-vue/src/views/DashboardPages/GamePage.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <input v-model="gameId" placeholder="Game ID" />
-    <input v-model="playerId" placeholder="Player ID" />
-    <button v-on:click="joinGame(gameId, playerId)">JOIN</button>
-    <hr />
     <input v-model="message" placeholder="Send a message" />
     <button v-on:click="sendMessage(gameId, playerId, message)">SEND</button>
     <hr />
-    <button v-on:click="startGame(gameId)">START GAME</button>
-    <div>{{questions}}</div>
+    <!-- <button v-on:click="startGame(gameId)">START GAME</button> -->
+    <button id="start" v-on:click="nextQuestion()" :disabled="currentQuestion>=10">START GAME</button>
+    <div v-if="started">
+      <h3 v-html="questions[currentQuestion-1].question"></h3>
+      <h1>{{currentTime}}</h1>
+    </div>
     <div>
       <ul>
         <li v-for="message in chatMessages" v-bind:key="message">{{ message }}</li>
@@ -18,17 +18,30 @@
 </template>
 
 <script>
+import { generateId } from "../../controllers/IdController";
+
 export default {
   data() {
     return {
-      gameId: undefined,
-      playerId: undefined,
-      questions: undefined,
+      gameId: this.$route.params.id,
+      playerId: generateId(),
+      currentTime: 0,
+      timer: undefined,
+      started: false,
+      questions: [],
+      currentQuestion: 1,
       chatMessages: [],
-      message: undefined
+      message: ""
     };
   },
-  mounted() {},
+  //   mounted() {
+  //     this.joinGame(this.gameId, this.playerId);
+  //   },
+  created() {
+    this.joinGame(this.gameId, this.playerId);
+    this.getQuestions(this.gameId);
+  },
+
   sockets: {
     connect() {},
     disconnect() {},
@@ -40,24 +53,51 @@ export default {
       this.chatMessages.push(data.playerId + ": " + data.message);
     },
     newQuestions(data) {
-        this.questions = data.questions;
+      for (var i = 0; i < data.questions.length; i++) {
+        this.questions.push(data.questions[i]);
+      }
     }
   },
   methods: {
+    countDownTimer() {
+      this.timer = setInterval(() => {
+        if (this.currentTime <= 0) {
+          clearInterval(this.timer);
+          if (this.currentQuestion < 10) {
+            this.nextQuestion();
+          }
+          // WE COULD SET STARTED TO FALSE HERE ON LAST LOOP
+          return;
+        }
+        this.currentTime--;
+      }, 1000);
+    },
     joinGame(gameId, playerId) {
-      // CHANGE THIS TO GET THE GAME ID PROVIDED FROM ROUTE
       this.$socket.emit("joinRequest", {
         gameId: gameId,
         playerId: playerId
       });
     },
-    startGame(gameId) {
-      this.getQuestions(gameId);
-    },
+    // startGame(gameId) {
+    //   this.getQuestions(gameId);
+    // },
     getQuestions(gameId) {
       this.$socket.emit("triviaRequest", {
         gameId: gameId
       });
+    },
+    nextQuestion() {
+      clearInterval(this.timer);
+      this.currentTime = 10;
+      this.countDownTimer();
+
+      if (!this.started) {
+        document.getElementById("start").innerHTML = "NEXT";
+        this.started = true;
+        return;
+      }
+      // WE COULD SET STARTED TO FALSE HERE ON LAST LOOP
+      this.currentQuestion++;
     },
     sendMessage(gameId, playerId, message) {
       this.$socket.emit("messageRequest", {
